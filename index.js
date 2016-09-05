93e7958d7f444b91d03dfb62570b58477c9a4173
Kathryn Schroeder <schroeder.kat@gmail.com>
2016-09-04 22:39:11 -0400

    Adding styling elements to buttons and profile picture

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 $(document).ready(function() {
-	// grab the initial top offset of the navigation 
+	//Grab the initial top offset of the navigation 
 	var stickyNavTop = $('.nav').offset().top;
 		   	
-	// our function that decides weather the navigation bar should have "fixed" css position or not.
+	//Function that decides weather the navigation bar should have "fixed" css position or not.
 	var stickyNav = function(){
 		var scrollTop = $(window).scrollTop(); // our current vertical position from the top
 		// if we've scrolled more than the navigation, change its position to fixed to stick to top,
 		// otherwise change it back to relative
-		if (scrollTop > stickyNavTop) { 
+
+		if (scrollTop > stickyNavTop) {
 			$('.nav').addClass('sticky');
 			$('#title').css('visibility', 'visible');
 		} else {
 			$('.nav').removeClass('sticky'); 
 			$('#title').css('visibility', 'hidden');
 		}
+
+
 	};
 
 	stickyNav();
-	// and run it again every time you scroll
+	//Run it again every time you scroll
 	$(window).scroll(function() {
 		stickyNav();
 	});
 
 	//Fix positioning of in-page anchor links
 	var shiftWindow = function() { scrollBy(0, -50) };
-if (location.hash) shiftWindow();
-window.addEventListener("hashchange", shiftWindow);
+	if (location.hash) shiftWindow();
+	window.addEventListener("hashchange", shiftWindow);
 
 	//Parallax
 	$(window).scroll(function(e){
@@ -37,6 +40,13 @@ window.addEventListener("hashchange", shiftWindow);
 	  $('.background').css('top',-(scrolled*0.2)+'px');
 	}
 
-	//Anchor link padding fix
-	window.addEventListener("hashchange", function() { scrollBy(0, -15) })
+	//Fade in navigation bar
+	// window.addEventListener("scroll", function() {
+	//     if (window.scrollY > 500) {
+	//         $('.wrapper').fadeIn();
+	//     }
+	//     else {
+	//         $('.wrapper').fadeOut();
+ //    	}
+	// },false);
 });
