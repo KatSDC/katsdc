2a8561ef3fd53a14a8f155d6ee4687ac3a0f0f0a
Kathryn Schroeder <schroeder.kat@gmail.com>
2016-09-08 22:28:10 -0400

    Added parallax to imaged for welcome and open source divs

--- index.js
+++ index.js
@@ -4,19 +4,15 @@ $(document).ready(function() {
 		   	
 	//Function that decides weather the navigation bar should have "fixed" css position or not.
 	var stickyNav = function(){
-		var scrollTop = $(window).scrollTop(); // our current vertical position from the top
-		// if we've scrolled more than the navigation, change its position to fixed to stick to top,
-		// otherwise change it back to relative
+		var scrollTop = $(window).scrollTop(); //Current vertical position from the top
 
 		if (scrollTop > stickyNavTop) {
 			$('.nav').addClass('sticky');
-			$('#title').css('visibility', 'visible');
+			$('.nav').fadeIn();
 		} else {
 			$('.nav').removeClass('sticky'); 
-			$('#title').css('visibility', 'hidden');
+			$('.nav').fadeOut();
 		}
-
-
 	};
 
 	stickyNav();
@@ -31,22 +27,6 @@ $(document).ready(function() {
 	window.addEventListener("hashchange", shiftWindow);
 
 	//Parallax
-	$(window).scroll(function(e){
-	  parallax();
-	});
-
-	function parallax(){
-	  var scrolled = $(window).scrollTop();
-	  $('.background').css('top',-(scrolled*0.2)+'px');
-	}
-
-	//Fade in navigation bar
-	// window.addEventListener("scroll", function() {
-	//     if (window.scrollY > 500) {
-	//         $('.wrapper').fadeIn();
-	//     }
-	//     else {
-	//         $('.wrapper').fadeOut();
- //    	}
-	// },false);
+	$('.background').parallax({imageSrc: 'images/waves.jpeg'});
+	$('.open-source').parallax({imageSrc: 'images/skyflowers.jpeg'});
 });
