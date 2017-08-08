1225476c46428b399059ab64b4e143945308bea0
Kathryn Schroeder <schroeder.kat@gmail.com>
2017-08-08 08:44:36 -0400

    Made some changes towards making the site responsive

--- index.js
+++ index.js
@@ -3,26 +3,26 @@ $(document).ready(function() {
 	var stickyNavTop = $('.nav').offset().top;
 		   	
 	//Function that decides whether the navigation bar should have "fixed" css position or not.
-	var stickyNav = function() {
-		var scrollTop = $(window).scrollTop() + 65; //Current vertical position from the top
+	// var stickyNav = function() {
+	// 	var scrollTop = $(window).scrollTop() + 65; //Current vertical position from the top
 
-		if (scrollTop > stickyNavTop) {
-			$('.nav').addClass('sticky');
-			$('.nav').fadeIn(400, function() {
-				//Parallax for open source section loads only after navbar is present to prevent offset issues
-				$('.open-source').parallax({imageSrc: 'images/skyflowers.jpeg'});
-			});
-		} else {
-			$('.nav').removeClass('sticky'); 
-			$('.nav').fadeOut(400);
-		}
-	}
+	// 	if (scrollTop > stickyNavTop) {
+	// 		$('.nav').addClass('sticky');
+	// 		$('.nav').fadeIn(400, function() {
+	// 			//Parallax for open source section loads only after navbar is present to prevent offset issues
+	// 			$('.open-source').parallax({imageSrc: 'images/skyflowers.jpeg'});
+	// 		});
+	// 	} else {
+	// 		$('.nav').removeClass('sticky'); 
+	// 		$('.nav').fadeOut(400);
+	// 	}
+	// }
 
-	stickyNav();
+	// stickyNav();
 	//Run it again every time you scroll
-	$(window).scroll(function() {
-		stickyNav();
-	});
+	// $(window).scroll(function() {
+	// 	stickyNav();
+	// });
 
 	//Fix positioning of in-page anchor links
 	var shiftWindow = function() { scrollBy(0, -50) };
